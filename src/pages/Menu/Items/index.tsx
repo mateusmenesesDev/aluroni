68ec5c482ba21b7afc323d1f06db3aaaf7ff609f
Mateus <mateusppa10@gmail.com>
2022-10-06 20:46:47 -0300

    Criando funcionalidade do order, filter e search

--- src/pages/Menu/Items/index.tsx
+++ src/pages/Menu/Items/index.tsx
@@ -1,10 +1,45 @@
+import { useEffect, useState } from 'react'
 import { Item } from './Item'
 import menu from './items.json'
 import style from './Items.module.scss'
-export function Items(){
+
+interface Props {
+    search: string,
+    filter: number | null,
+    order: string
+}
+export function Items({search, filter, order}: Props){
+    const[list, setList] = useState(menu) 
+    
+    function testSearch(title: string){
+        const regex = new RegExp(search, 'i')
+        return regex.test(title)
+    }
+
+    function testFilter(id: number){
+        if(filter!==null) return filter === id
+        return true
+    }
+    function ordenar(newList: typeof menu) {
+        switch(order) {
+          case 'porcao': 
+            return newList.sort((a, b) => a.size > b.size ? 1 : -1);
+          case 'qtd_pessoas':
+            return newList.sort((a,b) => a.serving > b.serving ? 1 : -1);
+          case 'preco':
+            return newList.sort((a,b) => a.price > b.price ? 1 : -1);
+          default:
+            return newList; 
+        }
+      }
+    useEffect(()=>{
+        
+        const newList = menu.filter(item => testSearch(item.title) && testFilter(item.category.id))
+        setList(ordenar(newList))
+    }, [search, filter, order])
     return(
         <div className={style.items}>
-            {menu.map(item=>(
+            {list.map(item=>(
                 <Item 
                     key={item.id} 
                     {...item}
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -28,7 +28,7 @@ export function Menu(){
                     <Filters filter={filter} setFilter={setFilter}/>
                     <Order order={order} setOrder={setOrder}/>
                 </div>
-                <Items/>
+                <Items search={search} filter={filter} order={order}/>
             </section>
         </main>
     )
